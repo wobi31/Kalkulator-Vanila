7c91479c755fcee4a8e86b6c37ef18478fae1648
Wobi Nurdiansyah <wobinurdiansyah20@gmail.com>
2025-01-25 07:07:04 +0700

    First commit on create

---
+++ VanilaKalkulator/index.js
@@ -0,0 +1,17 @@
+// function btn() {
+//   const conten = document.querySelector("#content");
+//   conten.textContent = "ini";
+//   conten.innerHTML = "baru";
+//   console.log("masuk");
+// }
+// function nAruto() {
+//   const conten = document.querySelector("#content");
+//   conten.textContent = "ini";
+//   conten.innerHTML = "baru";
+//   console.log("masuk");
+// }
+
+function masukOk(){
+  console.log('halo');
+  alert("halo")
+}
